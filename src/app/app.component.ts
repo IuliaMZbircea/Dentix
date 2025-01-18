b544f199544453474c4f0104a17c8a1bf890d587
IuliaMZbircea <iuliazbircea@gmail.com>
2025-01-18 22:23:53 +0200

    added bootstrap 5 and other components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'medical-record-platform';
+  title = 'Medical Record';
 }
